1e0c6c1f841496215bce50969776211c3aedc07d
renestimu <byumitsiz@gmail.com>
2022-06-30 18:10:30 +0300

    update2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
+import { registerLocaleData } from '@angular/common';
+import localeTr from '@angular/common/locales/tr';
+registerLocaleData(localeTr, 'tr');
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { NavComponent } from './nav/nav.component';
+import { ProductFilterPipe } from './product/product-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    NavComponent
+    NavComponent,
+    ProductFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  title = "Product List"
+  filterText = ""
+  products: Product[]=[
+    {id:1,name:"Product 1",description:"test2",createDate:Date.now(),price:1000,stock:10},
+    {id:2,name:"Product 2",description:"test2",createDate:Date.now(),price:200,stock:20},
+  ];
 
   ngOnInit(): void {
   }
